4253290dd9f825850f5f41935ce9bf0fdc8379c6
Yao J <kimmyoo@gmail.com>
2023-06-01 10:07:14 -0400

    changed it back to previous code

--- server.js
+++ server.js
@@ -30,10 +30,6 @@ app.use(express.static(__dirname + '/public'))
 app.use(cookieParser())
 
 // routes
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'index.html'))
-})
-
 app.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'))
     // another way of doing this
