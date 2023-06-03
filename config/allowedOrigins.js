9f3c6b5e0490591907d0e7b1694ad603fb4ad35b
Yao J <kimmyoo@gmail.com>
2023-06-03 15:23:04 -0400

    allowed origin changed

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,11 +1,11 @@
 const allowedOrigins = [
     // disable for deployment
-    // 'http://localhost:3000',
-    // 'http://localhost:3500',
+    'http://localhost:3000',
+    'http://localhost:3500',
 
     // added for deployment
-    'https://bejewelled-mermaid-b03bc6.netlify.app',
-    'https://vocab-nexus.onrender.com'
+    // 'https://bejewelled-mermaid-b03bc6.netlify.app',
+    // 'https://vocab-nexus.onrender.com'
 ]
 
 module.exports = allowedOrigins
