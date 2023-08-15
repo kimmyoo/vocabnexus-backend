ad97e0cbd249aaba0f7e8b0792b87c5dafaa6920
Yao J <kimmyoo@gmail.com>
2023-08-15 17:17:45 -0400

    changed allowed Origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     // disable for deployment
-    'http://localhost:3000',
-    'http://localhost:3500',
+    // 'http://localhost:3000',
+    // 'http://localhost:3500',
 
     // added for deployment
     // 'https://bejewelled-mermaid-b03bc6.netlify.app',
